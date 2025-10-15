6e1d1fd2c7ffe27431d851378a69288df2401b0e
Yurleis Zuluaga <yzuluagag1@unicartagena.edu.co>
2025-10-15 14:45:56 -0500

    fix: update login form to use correct field names and submit handler

--- resources/js/Pages/Login.vue
+++ resources/js/Pages/Login.vue
@@ -1,29 +1,29 @@
 <template>
-    <div class="login-page">
-    <div class="login-container">
-      <h2>Iniciar sesión</h2>
+  <div class="login-page">
+  <div class="login-container">
+    <h2>Iniciar sesión</h2>
 
-      <form @submit.prevent="form.post('/login')">
-        <div class="input-group">
-          <label for="usuario">Usuario</label>
-          <input id="usuario" v-model="form.nombre_usuario" type="text" placeholder="Ingresa tu usuario" />
-          <div v-if="form.errors.nombre_usuario" class="error">{{ form.errors.nombre_usuario }}</div>
-        </div>
-
-        <div class="input-group">
-          <label for="password">Contraseña</label>
-          <input id="password" v-model="form.password" type="password" placeholder="••••••••" />
-          <div v-if="form.errors.password" class="error">{{ form.errors.password }}</div>
-        </div>
-
-        <button type="submit">Entrar</button>
-      </form>
-
-        <div v-if="$page?.props?.flash?.error" class="error">
-        {{ $page.props.flash.error }}
-        </div>
-
+    <form @submit.prevent="submitForm">
+    <div class="input-group">
+      <label for="usuario">Usuario</label>
+      <input id="usuario" v-model="form.name" type="text" placeholder="Ingresa tu usuario" />
+      <div v-if="form.errors.name" class="error">{{ form.errors.name }}</div>
     </div>
+
+    <div class="input-group">
+      <label for="password">Contraseña</label>
+      <input id="password" v-model="form.password" type="password" placeholder="••••••••" />
+      <div v-if="form.errors.password" class="error">{{ form.errors.password }}</div>
+    </div>
+
+    <button type="submit">Entrar</button>
+    </form>
+
+    <div v-if="$page?.props?.flash?.error" class="error">
+    {{ $page.props.flash.error }}
+    </div>
+
+  </div>
   </div>
 </template>
 
@@ -33,7 +33,20 @@ import { useForm } from '@inertiajs/vue3'
 import '../../css/login.css'
 
 const form = useForm({
-  nombre_usuario: '',
+  name: '',
   password: ''
 })
+
+const submitForm = () => {
+  form.post('/login', {
+  onSuccess: () => {
+    alert('Inicio de sesión exitoso')
+  },
+  onError: (errors) => {
+    if (errors.password || errors.name) {
+    alert('Contraseña incorrecta')
+    }
+  }
+  })
+}
 </script>
