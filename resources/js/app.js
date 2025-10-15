667078f52c90ee45a463089fc56b845b07a7b519
Yurleis Zuluaga <yzuluagag1@unicartagena.edu.co>
2025-10-15 14:17:40 -0500

    feat: set up Inertia.js with initial app layout and entry point

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,15 +1,14 @@
-import './bootstrap'
-import '../css/app.css'
-import '../css/login.css'
-
-import { createApp, h } from 'vue'
-import { createInertiaApp } from '@inertiajs/vue3'
+import './bootstrap';
+import { createApp, h } from 'vue';
+import { createInertiaApp } from '@inertiajs/vue3';
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 
 createInertiaApp({
-  resolve: name => require(`./Pages/${name}.vue`),
+  resolve: (name) =>
+    resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
   setup({ el, App, props, plugin }) {
     createApp({ render: () => h(App, props) })
       .use(plugin)
-      .mount(el)
+      .mount(el);
   },
-})
+});
