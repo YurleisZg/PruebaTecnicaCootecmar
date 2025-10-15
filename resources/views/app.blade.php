8cc3c604025158866f0ff4f93207c9b772fe438c
Yurleis Zuluaga <yzuluagag1@unicartagena.edu.co>
2025-10-15 14:31:45 -0500

    feat: add UserSeeder class and update app title in main layout

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Mi aplicación Inertia</title>
+    <title>Prueba técnica</title>
     @vite('resources/js/app.js')
     @inertiaHead
   </head>
