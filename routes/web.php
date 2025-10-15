83717bc40e9864970d3a133b9fb722d74629c101
Yurleis Zuluaga <yzuluagag1@unicartagena.edu.co>
2025-10-15 13:47:12 -0500

    refactor: update routes to use AuthController for login and logout functionality

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\FormularioController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+// Login
+Route::get('/', [AuthController::class, 'loginForm'])->name('login');
+Route::post('/login', [AuthController::class, 'login'])->name('login.post');
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
+// Rutas protegidas
+Route::middleware(['checkauth'])->group(function () {
+    Route::get('/formulario', [FormularioController::class, 'index'])->name('formulario');
 });
